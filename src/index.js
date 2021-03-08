af5c8ff857d0274e104b89065d24cff87b34d021
Martin Renteria <renteriamp96@gmail.com>
2021-03-08 11:07:08 -0800

    Reconfigured file structure and server is complied

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const reactContentRoot = document.getElementById('root')
+  
+ReactDOM.render(<App />, reactContentRoot);
