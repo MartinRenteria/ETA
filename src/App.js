e99b2744c307b66512de896779069e99df6c3b34
Truc Nguyen <trucn0215@gmail.com>
2021-03-13 11:51:56 -0800

    create Signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 import Login from "./components/Login/Login"
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
     <Jumbotron>
+      <h1>Login</h1>
       <Login />
+      <br></br>
+      <h1>Signup</h1>
+      <Signup/>
     </Jumbotron>
 
   )
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -9,7 +9,7 @@ export default function Login () {
                 <Form.Control type="email" placeholder="Enter email" />
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
-    </Form.Text>
+                </Form.Text>
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
