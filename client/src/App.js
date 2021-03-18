ee58b182b7b7e4dca7ea29c01c807aec4d878fab
Truc Nguyen <trucn0215@gmail.com>
2021-03-17 20:34:26 -0700

    create employee function

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
-import Login from "../src/components/Login/Login"
-import Signup from '../src/components/Signup/Signup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavButton from '../src/components/Navbar/NavButton';
 import { useAuthTokenStore } from "./utils/auth";
 import RegistrationForm from '../src/components/RegistrationForm';
-import { BrowserRouter} from "react-router-dom";;
+import { BrowserRouter } from "react-router-dom";;
 
 function App() {
 
@@ -14,20 +12,13 @@ function App() {
 
   return (
     <BrowserRouter>
-    
-    <Jumbotron className="m-0 p-0">
-      <NavButton/>
 
-      <h1>Login</h1>
-      <Login />
+      <Jumbotron className="m-0 p-0">
+        <NavButton />
 
-      <br></br>
-      <h1>Sign up</h1>
-      <Signup/>
+        <RegistrationForm />
 
-     <RegistrationForm />
-      
-    </Jumbotron>
+      </Jumbotron>
     </BrowserRouter>
 
   )
