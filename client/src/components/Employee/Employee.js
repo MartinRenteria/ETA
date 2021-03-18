c15734e1cd7c04a4ae00f9e8e0eb65c22bc35b93
Truc Nguyen <trucn0215@gmail.com>
2021-03-18 00:22:44 -0700

    filling to complete working hours of the day

--- client/src/components/Employee/Employee.js
+++ client/src/components/Employee/Employee.js
@@ -3,9 +3,9 @@ import { Card, Table } from "react-bootstrap"
 import InAndOut from "../Clock-In-Out-Button/InAndOut"
 import EndDay from "../End-Day-Button/EndDay"
 
-export default function Employee () {
+export default function Employee() {
     return (
-        <Card className="mx-auto bg-info my-2" style = {{width: "60%", height: "60%"}}>
+        <Card className="mx-auto bg-info my-2" style={{ width: "60%", height: "60%" }}>
             <div className="mx-auto my-2">
                 {/* Clock In and Out button */}
                 <InAndOut />
@@ -34,13 +34,34 @@ export default function Employee () {
                         <td>12:00 PM</td>
                         <td>4</td>
                     </tr>
+                    <tr className="text-danger fw-bold">
+                        <td colSpan="4" >Lunch</td>
+                    </tr>
+                    <tr>
+                        <td>Clock in</td>
+                        <td>March 17, 2021</td>
+                        <td>12:30 AM</td>
+                        <td>0</td>
+                    </tr>
+                    <tr>
+                        <td>Clock Out</td>
+                        <td>March 17, 2021</td>
+                        <td>16:30 PM</td>
+                        <td>4</td>
+                    </tr>
+                    <tr className="text-danger fw-bold">
+                        <td colSpan="3" >Total Worked Hours</td>
+                        <td>8</td>
+                    </tr>
                 </tbody>
             </Table>
 
+
+
             {/* End Day button */}
             <div className="d-flex justify-content-end m-1">
-                    <EndDay />
-                </div>
+                <EndDay />
+            </div>
         </Card>
 
 
