bd607a80eae3b5c0f4c7074ee9fe1bd05bb443bf
Truc Nguyen <trucn0215@gmail.com>
2021-03-20 12:36:04 -0700

    create time clock for employee

---
+++ client/src/components/CurrentTime/CurrentTime.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import Clock from 'react-clock';
+import "react-clock/dist/Clock.css"
+
+function CurrentTime() {
+
+const [value, setValve] = useState(new Date())
+
+console.log("Time: " + value)
+
+useEffect(()=> {
+    const interval = setInterval(() => setValve(new Date()), 1000);
+
+    return () => {
+        clearInterval(interval)
+    };
+}, []);
+
+    return (
+        <div className="m-2">
+            <Clock value={value} />
+        </div>
+    )
+}
+
+export default CurrentTime;
