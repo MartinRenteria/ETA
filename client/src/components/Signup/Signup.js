dbbacb1b31bcb8ae6744c3ba99b86df442004644
Martin Renteria <renteriamp96@gmail.com>
2021-03-21 12:57:41 -0700

    Set-up registration form again

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -4,44 +4,6 @@ import api from "../../utils/api";
 import { Form, Button, Modal } from "react-bootstrap";
 
 export default function Signup(props) {
-    return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Body>
-                <Form>
-                    <Form.Group controlId="exampleForm.ControlInput1">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control type="Text" placeholder="Enter First Name" />
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlInput1">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="Text" placeholder="Enter Last Name" />
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlInput1">
-                        <Form.Label>Email Address</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Enter Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlSelect1">
-                        <Form.Label>Employee Title</Form.Label>
-                        <Form.Control type="Text" placeholder="Enter title" />
-                    </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlSelect4">
-                        <Form.Label>Are you an invidivudal contributor?</Form.Label>
-                        <Form.Control type="checkbox" />
-                    </Form.Group>
-                    {/* commenting out for mvp
 
   const emailRef = useRef();
   const passwordRef = useRef();
@@ -77,6 +39,44 @@ export default function Signup(props) {
 
       }
   }
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Body>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>First Name</Form.Label>
+                        <Form.Control type="Text" placeholder="Enter First Name" />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Last Name</Form.Label>
+                        <Form.Control type="Text" placeholder="Enter Last Name" />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control type="email" ref={emailRef} placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Enter Password</Form.Label>
+                        <Form.Control type="password" ref={passwordRef} placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Label>Employee Title</Form.Label>
+                        <Form.Control type="Text" placeholder="Enter title" />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlSelect4">
+                        <Form.Label>Are you an invidivudal contributor?</Form.Label>
+                        <Form.Control type="checkbox" />
+                    </Form.Group>
+                    {/* commenting out for mvp
   
   return (
     <Modal
